5b9f94519f77474cd216c7bcf0f3ce7284a42854
alevis <lagaba@umass.edu>
2017-01-17 23:33:18 -0500

    tic tac toe works.

--- tictac/script.js
+++ tictac/script.js
@@ -1,52 +1,41 @@
-var board = [0,0,0,0,0,0,0,0,0];
 function state(b){
-/*
-    //solution space is size 8
-    if(board[0]&&board[1]&&board[2]){
-        return true;
-    } else if(board[]){
-        return true;
-    } else if(){
-        return true;
-    } else if(){
-        return true;
-    } else if(){
-        return true;
-    } else if(){
-        return true;
-    } else if(){
-        return true;
-    } else if(){
-        return true;
-    } else{
-        return false;
-    }
-*/
-return (b[0]&&b[1]&&b[2])||(b[0]&&b[4]&&b[8])||(b[0]&&b[3]&&b[6])||
+ return (b[0]&&b[1]&&b[2])||(b[0]&&b[4]&&b[8])||(b[0]&&b[3]&&b[6])||
         (b[1]&&b[4]&&b[7])||(b[2]&&b[5]&&b[8])||(b[2]&&b[4]&&b[6])||
         (b[3]&&b[4]&&b[5])||(b[6]&&b[7]&&b[8]);
-}
-function ai_move(cell_number){
-    var ai = Math.floor(Math.random()*10);
-    return ai != cell_number ? ai : ai_move(cell_number);
 }
 function remove(marks,number){
     var acpy = [];
     marks.forEach(function(element){
         if(element!=number)
             acpy.push(element);
-    });
-    return acpy;
+    }); return acpy;
+}
+function game_state(player,board,marks){
+    if(state(board)){
+        alert(player+" wins!");
+        location.reload();
+    } else if(marks.length == 0){
+        alert("Draw!");
+        location.reload();
+    }
 }
 var marks = [0,1,2,3,4,5,6,7,8];
+var human_board = [0,0,0,0,0,0,0,0,0];
+var robot_board = [0,0,0,0,0,0,0,0,0];
 $(document).ready(function(){
-    $("#tic").click(function(event){
+    var ai;
+    $("#tic,#tac,#toe").click(function(event){
         event.target.innerHTML="X";
-        remove(marks,event.target.id);
-        board[event.target.id]=1;
-        if(state(board)){
-            alert("X wins!");
-            event.defaultPrevented();
-        } else{ $("#"+ai_move(event.target.id)).append("0"); }
+        marks=remove(marks,parseInt(event.target.id));
+        human_board[parseInt(event.target.id)] = 1;
+        game_state("X",human_board,marks);
+         
+        ai = marks[Math.floor(Math.random()*marks.length)];
+        $("#"+ai).append("0");
+        marks=remove(marks,ai);
+        robot_board[ai] = 1;
+        game_state("0",robot_board,marks);
+        
     });
+    $(".heading").click(function(){ location.reload();  });
 });
