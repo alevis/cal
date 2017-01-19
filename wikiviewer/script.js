2baaa10d6f916e9d1bdaf6a32d6ff4ced35ef631
alevis <lagaba@umass.edu>
2017-01-18 23:52:32 -0500

    before it's too late!

--- wikiviewer/script.js
+++ wikiviewer/script.js
@@ -12,7 +12,16 @@ var p = "";
 var close = "";
 var with_links = "";
 function qparse(search_string,link_param){
-    var r1 = "https://en.wikipedia.org/w/api.php?"
+    var r1 = "https://en.wikipedia.org/w/api.php?";
+    var all = "action=query&generator=allpages&";
+    var from = "gaplimit=5&gapfrom=";
+    var end = "&prop=";
+}
+// direct search function
+function dparse(search_string){
+    var s1 = "http://en.wikipedia.org/w/api.php?";
+    var s2 = "format=json&action=query&prop=extracts&";
+    var s3 = "exintro=&explaintext=&titles=";
 }
 $(document).ready(function(){
 
