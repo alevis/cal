946589cb84d834d306da308c39d1d501f47e1ab9
alevis <lagaba@umass.edu>
2017-01-18 23:49:21 -0500

    message

--- wikiviewer/script.js
+++ wikiviewer/script.js
@@ -1,3 +1,19 @@
+var results = [];
+var keys = [];
+var key_size;
+var resp_obj;
+var resp_title="";
+var resp_extract = "";
+var search_string = "";
+// List item variables
+var b = "";
+var h = "";
+var p = "";
+var close = "";
+var with_links = "";
+function qparse(search_string,link_param){
+    var r1 = "https://en.wikipedia.org/w/api.php?"
+}
 $(document).ready(function(){
 
 });
