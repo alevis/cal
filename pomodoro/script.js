45b78545f5db54bd36400546eab9b2278951a9fc
alevis <lagaba@umass.edu>
2017-01-30 21:40:53 -0500

    added javascript

--- pomodoro/script.js
+++ pomodoro/script.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-
+    var ctx = $('#canvas').getContext('2d');
+    ctx.lineWidth = 23;
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    ctx.font = '25px Trebuchet MS';
+    ctx.fillStyle='black';
 });
